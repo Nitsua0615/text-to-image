cfae8dead0ed2ee1ee579fe1acd2c3c8cb2b5b80
Nitsua0615 <45273698+Nitsua0615@users.noreply.github.com>
2023-09-24 04:25:04 +0000

    m

--- rqtest.js
+++ rqtest.js
@@ -1,4 +1,4 @@
-const res = await fetch("https://raw.githubusercontent.com/Nitsua0615/text-to-image/main/test.png");
+const res = await fetch("https://raw.githubusercontent.com/Nitsua0615/text-to-image/main/2.png");
 const arb = await res.arrayBuffer();
 
 console.log(arb);
