56f45d9dff6f2dca8e208917b2a52fe12ceebd6a
Nitsua0615 <45273698+Nitsua0615@users.noreply.github.com>
2023-09-24 11:57:38 +0000

    test

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ import { createWriteStream, readFileSync, createReadStream } from "fs";
 import { PNG } from "pngjs";
 
 function encodeTextToImage(data, filename) {
-    const width = Math.ceil(Math.sqrt(data.length / 4));
-    const height = Math.ceil(data.length / (4 * width));
-    const png = new PNG({ width, height });
-
-    for (let i = 0; i < data.length; i += 4) {
-        for (let offset = 0; offset < 4; offset++) {
-            const dataIndex = ((png.width * Math.floor(i / (4 * width)) + (i / 4) % width) << 2) + offset;
-            if (dataIndex >= data.length) continue;
-            png.data[dataIndex] = data.readUInt8(i + offset);
+    return new Promise((resolve, reject) => {
+        const width = Math.ceil(Math.sqrt(data.length / 4));
+        const height = Math.ceil(data.length / (4 * width));
+        const png = new PNG({ width, height });
+    
+        for (let i = 0; i < data.length; i += 4) {
+            for (let offset = 0; offset < 4; offset++) {
+                const dataIndex = ((png.width * Math.floor(i / (4 * width)) + (i / 4) % width) << 2) + offset;
+                if (dataIndex >= data.length) continue;
+                png.data[dataIndex] = data.readUInt8(i + offset);
+            }
         }
-    }
-    png.pack().pipe(createWriteStream(filename));
+        
+        png.pack().pipe(createWriteStream(filename)).on("finish", () => {
+            resolve(true);
+        });
+    });
 }
 
 const data = readFileSync("./test.txt");
 // const data = Buffer.from("This is a test");
 
-encodeTextToImage(data, "output.png");
+await encodeTextToImage(data, "output.png");
 
 createReadStream("output.png").pipe(new PNG()).on("parsed", (data) => {
     console.log(data, data.toString());
