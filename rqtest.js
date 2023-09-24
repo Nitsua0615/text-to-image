69adf77cda16fefca5a5a5399f64e1223a25d787
Nitsua0615 <45273698+Nitsua0615@users.noreply.github.com>
2023-09-24 02:03:40 +0000

    update to remove png header

--- rqtest.js
+++ rqtest.js
@@ -1,4 +1,4 @@
-const res = await fetch("https://3000-nitsua0615-textimage-v455gz11ywu.ws-us104.gitpod.io/test.png");
+const res = await fetch("https://raw.githubusercontent.com/Nitsua0615/text-to-image/main/test.png");
 const arb = await res.arrayBuffer();
 
-console.log(arb);
+console.log(arb.slice(61, arb.byteLength));
