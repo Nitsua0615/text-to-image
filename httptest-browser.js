4a091e5cb187e56dea94e44757b7821c4bf249d7
wolfy01 <nitsua0615+github@gmail.com>
2023-09-26 18:34:51 -0400

    boom, finished

--- httptest-browser.js
+++ httptest-browser.js
@@ -1,40 +1,14 @@
-// load https://cdn.jsdelivr.net/npm/pngjs/browser.min.js
-const PNG = png.PNG;
+let img = new Image();
+img.crossOrigin = "Anonymous";
+img.src = "https://raw.githubusercontent.com/Nitsua0615/text-to-image/main/output.png";
 
-const imageUrl = 'https://raw.githubusercontent.com/Nitsua0615/text-to-image/main/output.png';
+img.onload = async () => {
+    if (!window.canvas) window.canvas = document.createElement("canvas");
+    if (!window.ctx) window.ctx = window.canvas.getContext("2d");
+    
+    ctx.drawImage(img, 0, 0);
 
-function downloadImage(url) {
-    return new Promise((resolve, reject) => {
-        fetch(url)
-            .then(response => response.arrayBuffer())
-            .then(buffer => {
-                resolve(new Uint8Array(buffer));
-            })
-            .catch(reject);
-    });
-}
+    const filteredData = new TextDecoder("utf8").decode(Uint8Array.from(ctx.getImageData(0, 0, img.width, img.height).data.filter(b => b !== 255 && b !== 0)));
 
-function parseImage(buffer) {
-    return new Promise((resolve, reject) => {
-        new PNG().parse(buffer, (error, data) => {
-            if (error) {
-                reject(error);
-                return;
-            }
-
-            resolve(data);
-        });
-    });
-}
-
-downloadImage(imageUrl)
-    .then(imageBuffer => parseImage(imageBuffer))
-    .then(imageData => {
-        console.log('Image width:', imageData.width);
-        console.log('Image height:', imageData.height);
-        console.log('Pixel data:', imageData.data);
-        console.log('To text:', imageData.data.toString().replaceAll(String.fromCharCode(0), ""));
-    })
-    .catch(error => {
-        console.error(`Error: ${error.message}`);
-    });
+    console.log(filteredData);
+};
--- index.js
+++ index.js
@@ -1,74 +1,42 @@
 import { createWriteStream, readFileSync, createReadStream } from "fs";
 import { PNG } from "pngjs";
 
-function readColor(buffer, index, num = 4) {
-    let rgbOa = [];
-
-    for (let offset = 0; offset < num; offset++) {
-        try {
-            rgbOa.push(buffer.readUInt8(index + offset));
-        } catch (e) {}
-    }
-
-    return rgbOa;
-}
-
 function encodeTextToImage(data, filename) {
     return new Promise((resolve) => {
         const pixelCount = Math.ceil(data.length / 3);
         let targetWidth = Math.ceil(Math.sqrt(pixelCount));
-        let minNulls = (targetWidth * Math.ceil(pixelCount / targetWidth)) - pixelCount;
-        
+
         for (let width = targetWidth; width > 1; --width) {
-          let height = Math.ceil(pixelCount / width);
-          let nulls = (width * height) - pixelCount; // guaranteed to be >= 0 because of Math.ceil
-          if (nulls < minNulls) {
-            minNulls = nulls;
-            targetWidth = width;
-            if (nulls === 0) break; // we can't have nulls equal to 0 without it being less than minNulls
-          }
+            if (width * Math.ceil(pixelCount / width) - pixelCount < targetWidth * Math.ceil(pixelCount / targetWidth) - pixelCount) {
+                targetWidth = width;
+            }
+            if (width * Math.ceil(pixelCount / width) - pixelCount === 0) break;
         }
-        
-        const targetHeight = Math.round(pixelCount / targetWidth); // just in case. i fucking hate IEEE 754
 
-        const png = new PNG({ 
-            width: targetWidth, 
-            height: targetHeight
-        });
-    
-        let dataIdx = 0;
-        for (let i = 0; i < data.length; i += 3) {
-            const [r, g, b] = readColor(data, i, 3);
+        const targetHeight = Math.round(pixelCount / targetWidth);
+        const png = new PNG({ width: targetWidth, height: targetHeight });
 
+        for (let dataIdx = 0, i = 0; i < data.length; i += 3, dataIdx++) {
+            const [r, g, b] = data.slice(i, i + 3);
             const idx = (png.width * (Math.floor(dataIdx / targetWidth)) + (dataIdx % targetWidth)) << 2;
             png.data[idx] = r;
             png.data[idx + 1] = g;
             png.data[idx + 2] = b;
             png.data[idx + 3] = 255;
-            dataIdx++;
         }
-        
+
         png.pack().pipe(createWriteStream(filename)).on("finish", () => {
-            console.log("Width:", png.width);
-            console.log("Height:", png.height);
-            console.log("Data:", png.data);
-            console.log("Gamma:", png.gamma);
-            console.log("Writable:", png.writable);
-            console.log("Readable:", png.readable);
-            console.log("Text:", png.data.toString());
+            console.log(`Width: ${png.width}\nHeight: ${png.height}\nData: ${Buffer.from(png.data).filter(b => b !== 255 && b !== 0).toString()}`);
             resolve(true);
         });
     });
 }
 
 const data = readFileSync("./test.txt");
-// const data = Buffer.from("This is a test");
-
 await encodeTextToImage(data, `output.png`);
 
-createReadStream("output.png").pipe(new PNG()).on("parsed", (data) => {
-    console.log("-");
-    console.log("Load:");
-    console.log(data);
-    console.log(data.toString().replaceAll(String.fromCharCode(65533), ""));
-});
+createReadStream("output.png")
+    .pipe(new PNG())
+    .on("parsed", (data) => {
+        console.log("-\nLoad:\n" + Buffer.from(data).filter(b => b !== 255 && b !== 0).toString());
+    });
